599fb3bcd84b66d8e29e316338d93b7444df6267
Yangchen Shen <shyangch@gmail.com>
2018-06-10 13:12:57 -0700

    Enable progress bar changes

--- 11 - Custom Video Player/scripts-ys.js
+++ 11 - Custom Video Player/scripts-ys.js
@@ -27,6 +27,16 @@ function handleRangeUpdate() {
   // name is volumn or playbackRate
 }
 
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+}
+
 /* Hook up the event listeners */
 video.addEventListener('click', togglePlay);
 toggle.addEventListener('click', togglePlay);
@@ -38,3 +48,11 @@ skipButtons.forEach(button => button.addEventListener('click', skip));
 
 ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
 ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+
+video.addEventListener('timeupdate', handleProgress);
+
+let mousedown = false;
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
