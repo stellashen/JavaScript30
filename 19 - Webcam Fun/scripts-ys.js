e20def0f77a7ef36ccdb4010230dbc228d5a4d19
Yangchen Shen <shyangch@gmail.com>
2018-08-18 23:08:33 -0700

    paintToCanvas

--- 19 - Webcam Fun/scripts-ys.js
+++ 19 - Webcam Fun/scripts-ys.js
@@ -16,4 +16,28 @@ function getVideo() {
     });
 }
 
+function paintToCanvas() {
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+  canvas.width = width;
+  canvas.height = height;
+
+  return setInterval(() => {
+    // start from top left corner
+    ctx.drawImage(video, 0, 0, width, height);
+    // take the pixels out
+    let pixels = ctx.getImageData(0, 0, width, height);
+    // mess with them
+    // pixels = redEffect(pixels);
+
+    pixels = rgbSplit(pixels);
+    // ctx.globalAlpha = 0.8;
+
+    // pixels = greenScreen(pixels);
+    // put them back
+    ctx.putImageData(pixels, 0, 0);
+  }, 16);
+  // paint to canvas every 16 ms
+}
+
 getVideo();
